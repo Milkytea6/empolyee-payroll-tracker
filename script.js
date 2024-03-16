649b02a29ca1a380f7cd24bac215ce5964fa8e56
Christopher Giordano <chrisgcpg@gmail.com>
2024-03-16 16:55:26 -0400

    Made some changes to the if statement in the collectEmployees function to be able to add additional employees to the employeesArray.

--- script.js
+++ script.js
@@ -1,51 +1,66 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
+  
 
-// Array of empolyees
-let employeesArray = [];
-
-// Collect employee data
-const collectEmployees = function() {
+const collectEmployees = function () {
   // TODO: Get user input to create and return an array of employee objects
+  
+  // Array of empolyees
+  let employeesArray = [];
 
   // When the user clicks the "Add Employees" button, input windows of first name, 
   // last name, and salary display allowing user to input data into each window.
   let inputFirstName = window.prompt("Enter employee's first name.")
   let inputLastName = window.prompt("Enter employee's last name.")
-  let inputSalary = window.prompt("Enter empolyee's slaray.")
-
-// Store inputs into array of employees
+  let inputSalary = parseInt(window.prompt("Enter empolyee's slaray."))
+  
+  // Collect employee data
+  // Store inputs into Employee object.
   let newEmployee = {
     firstName: inputFirstName,
     lastName: inputLastName,
     salary: inputSalary
   };
+  
+  // Pushes Employee into employeesArray.
   employeesArray.push(newEmployee);
-
-// Then another input window will display asking if the user would like to continue adding or stop adding employees.
-let inputContinue = window.confirm("Add another employee?");
-
-if (inputContinue) {
-  collectEmployees()
-}
-  else{
-    return;
+  
+  // Then another input window will display asking if the user would like to continue adding or stop adding employees.
+  let inputContinue = window.confirm("Add another employee?");
+  
+  if (inputContinue) {
+    // Allows employees able to be addded to newly pushed array.
+    let additionalEmployees = collectEmployees();
+    // Concatenates the employeesArray with additionalEmployees.
+    employeesArray = employeesArray.concat(additionalEmployees);
+  }
+  else {
+    return employeesArray;
   }
 };
 
-  // Then the data will be stored and displayed in the HTML table in a row sorted alphabetically by last name.
-
-// addEmployeesBtn.addEventListener("click", function() {
-//   collectEmployees();
-// })
+// Then the data will be stored and displayed in the HTML table in a row sorted alphabetically by last name.
+// <<SHOULD HAPPEN IN STATER CODE LINE 102>>
 
 // Display the average salary
-const displayAverageSalary = function(employeesArray) {
+const displayAverageSalary = function (employeesArray) {
   // TODO: Calculate and display the average salary
+
+  // Access each object in employeesArray and get newEmployee.salary key.
+  // Average all the values in newEmployee.salary and console log the result.
+
+
+  // const salaries = employeesArray.inputSalary;
+
+  // const sum = salaries.reduce((acc, curr) => acc + curr, 0);
+  // const average = (sum/employeesArray.length);
+  // console.log(average);
+
+
 }
 
 // Select a random employee
-const getRandomEmployee = function(employeesArray) {
+const getRandomEmployee = function (employeesArray) {
   // TODO: Select and display a random employee
 }
 
@@ -56,7 +71,7 @@ const getRandomEmployee = function(employeesArray) {
 */
 
 // Display employee data in an HTML table
-const displayEmployees = function(employeesArray) {
+const displayEmployees = function (employeesArray) {
   // Get the employee table
   const employeeTable = document.querySelector('#employee-table');
 
@@ -79,9 +94,9 @@ const displayEmployees = function(employeesArray) {
 
     const salaryCell = document.createElement("td");
     // Format the salary as currency
-    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
-      style:"currency",
-      currency:"USD"
+    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD"
     });
 
     newTableRow.append(salaryCell);
@@ -90,7 +105,7 @@ const displayEmployees = function(employeesArray) {
   }
 }
 
-const trackEmployeeData = function() {
+const trackEmployeeData = function () {
   const employees = collectEmployees();
 
   console.table(employees);
@@ -101,7 +116,7 @@ const trackEmployeeData = function() {
 
   getRandomEmployee(employees);
 
-  employees.sort(function(a,b) {
+  employees.sort(function (a, b) {
     if (a.lastName < b.lastName) {
       return -1;
     } else {
@@ -114,3 +129,7 @@ const trackEmployeeData = function() {
 
 // Add event listener to 'Add Employees' button
 addEmployeesBtn.addEventListener('click', trackEmployeeData);
+
+
+
+
